65d42f1dc15ab577013d0475a970be35cd5937b8
rylichtig <rlichtig@gmail.com>
2014-12-16 02:15:38 -0800

    Getting Started

---
+++ NBodyVisualizer/src/nbodyvisualizer/NBodyVisualizer.java
@@ -0,0 +1,18 @@
+
+
+package nbodyvisualizer;
+
+/**
+ *
+ * @author Ryan Lichtig
+ */
+public class NBodyVisualizer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("This is a first time for me - it's awesome.");
+    }
+    
+}
